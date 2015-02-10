022038178f4f3b3d8f10632d266a8033f5445c74
Kamil Szymański <kszymanski@kaliop.com>
2015-02-10 23:09:27 +0100

    Initial commit.

---
+++ ezinfo.php
@@ -0,0 +1,34 @@
+<?php
+//
+// ## BEGIN COPYRIGHT, LICENSE AND WARRANTY NOTICE ##
+// SOFTWARE NAME: KS Admin Tabs
+// SOFTWARE RELEASE:  0.1-alpha
+// COPYRIGHT NOTICE: Copyright (C) 2015 Kamil Szymański
+// SOFTWARE LICENSE: GNU General Public License v2
+// NOTICE: >
+//   This program is free software; you can redistribute it and/or
+//   modify it under the terms of version 2.0  of the GNU General
+//   Public License as published by the Free Software Foundation.
+// 
+//   This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//   GNU General Public License for more details.
+// 
+//   You should have received a copy of version 2.0 of the GNU General
+//   Public License along with this program; if not, write to the Free
+//   Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+//   MA 02110-1301, USA.
+// ## END COPYRIGHT, LICENSE AND WARRANTY NOTICE ##
+//
+
+class ksadmintabssInfo
+{
+    static function info()
+    {
+        return array( 'Name'      => '<a href="http://github.com/eeree">KS Admin Tab</a> extension',
+                      'Version'   => '0.1-alpha',
+                      'Copyright' => 'Copyright (C) 2015 Kamil Szymański',
+                      'License'   => 'GNU General Public License v2.0' );
+    }
+}
---
+++ settings/admininterface.ini.append.php
@@ -0,0 +1,15 @@
+<?php /* #?ini charset="utf-8"?
+
+[WindowControlsSettings]
+AdditionalTabs[]=customtab
+
+[AdditionalTab_customtab]
+Title=Example Tab
+Description=Some example tab description
+NavigationPartName=ezcontentnavigationpart
+ContentClassID=21
+ContentNodeID=60
+ParentNodeID=2
+ContentObjectID=58
+ContentClassIdentifier=landing_page
+
